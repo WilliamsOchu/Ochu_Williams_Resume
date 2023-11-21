5757cac1c43bb930c017e7d19cc1eeb21a77917c
unplugged001 <ochuwilliamsirawo@gmail.com>
2023-11-21 07:41:21 +0100

    added abz

--- CRC_azure_fxn/__init__.py
+++ CRC_azure_fxn/__init__.py
@@ -19,6 +19,6 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
         return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
     else:
         return func.HttpResponse(
-             "Lace dem trainers up by headie one and ktrapp, bandokay",
+             "Lace dem trainers up by headie one and ktrapp, bandokay and abracadabra",
              status_code=200
         )
