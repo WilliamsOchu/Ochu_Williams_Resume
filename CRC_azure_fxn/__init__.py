d19d13db3277be495d6f31d71fee0d7c94c86284
unplugged001 <ochuwilliamsirawo@gmail.com>
2023-11-21 07:25:18 +0100

    added git workflows

--- CRC_azure_fxn/__init__.py
+++ CRC_azure_fxn/__init__.py
@@ -19,6 +19,6 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
         return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
     else:
         return func.HttpResponse(
-             "Lace dem trainers up",
+             "Lace dem trainers up by headie one and ktrapp",
              status_code=200
         )
